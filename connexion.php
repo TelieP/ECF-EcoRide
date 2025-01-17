4c5611257b6e21bbdbcaad68a4859cb9ede7ed44
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-01-17 11:13:48 +0100

    connexion

--- connexion.php
+++ connexion.php
@@ -5,38 +5,37 @@ require_once 'includes/connect.php';
 
 if (!empty($_POST)) {
 
-    if (isset($_POST["email"], $_POST['mot_de_passe'])) {
+    $email = $_POST["email"];
+    $password = $_POST['mot_de_passe'];
 
+    if (isset($email, $password)) {
         //on verifie que le mail est un vrai mail
-        if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-
-            die('Adresse email invalide');
-        }
+        // if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        //     die('Adresse email invalide' . $email);
+        // }
         // on se connecte à la base de données
-
-
         // on prepare la requete
         $sql = "SELECT * FROM `utilisateur` WHERE `email` = :email";
         $query = $conn->prepare($sql);
-        $query->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+        $query->bindValue(':email', $email, PDO::PARAM_STR);
         $query->execute();
         $user = $query->fetch(PDO::FETCH_ASSOC);
         if (!$user) {
             die('Utilisateur inconnu');
         }
-        if (!password_verify($_POST['mot_de_passe'], $user['password'])) {
-            die('Mot de passe incorrect');
+        if (!password_verify($password, $user['password'])) {
+            echo '<script type="text/javascript">window.alert("Mot de passe incorrect");</script>';
+        } else {
             $_SESSION['user'] = $user;
             header('Location: index.php');
-            exit();
         }
     }
 }
 ?>
 
 <form method="post">
-    Email: <input type="email" name="email" required><br>
-    Mot de passe: <input type="password" name="mot_de_passe" required><br>
+    Email: <input type="email" name="email" id="email" required><br>
+    Mot de passe: <input type="password" name="mot_de_passe" id="mot_de_passe" required><br>
     <button type="submit">Se connecter</button>
 </form>
 
