22bfd9c88edcee3878341c3c6cc86f13a27be779
PARFAIT <parfaitteli@outlook.fr>
2025-01-19 19:27:01 +0100

    mis à jour fichier inscription

--- inscription.php
+++ inscription.php
@@ -40,8 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt->bindParam(':photo', $photo, PDO::PARAM_STR);
             $stmt->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
             if ($stmt->execute()) {
-                $statement = $db->prepare('INSERT INTO `possede`(Id_role, Id_utilisateur) VALUES($_POST['Id_role'], $conn->lastInsertId())');
-                $statement->execute();
+
 
                 echo "Inscription réussie !";
                 header('Location: profil.php');
@@ -66,19 +65,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     Date de naissance: <input type="date" name="naissance" required><br><br>
     Photo:: <input type="file" name="photo" accept="image/png , image/jpeg"><br><br>
     Pseudo: <input type="text" name="pseudo" required><br><br>
-<<<<<<< HEAD
-    <label for="role">Role :</label>
-=======
->>>>>>> a95781c91c044ec2ded0ca36e1c0b10f7381d0f0
+
+
     <?php
     $stmt = $conn->prepare("SELECT Id_role, libelle FROM role");
     $stmt->execute();
     $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
-<<<<<<< HEAD
-=======
     <label for="role">Role :</label>
->>>>>>> a95781c91c044ec2ded0ca36e1c0b10f7381d0f0
+
     <select name="role" id="role">
         <?php
         foreach ($roles as $role) {
@@ -87,14 +82,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php
         }
         ?>
-<<<<<<< HEAD
+
     </select> <br><br>
 
     </select> <br><br>
-=======
+
     </select>
 
 
->>>>>>> a95781c91c044ec2ded0ca36e1c0b10f7381d0f0
+
     <button type="submit">S'inscrire</button>
 </form>
