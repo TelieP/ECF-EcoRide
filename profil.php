a230347f51b309dd102d5307de75e739cc17b2c1
PARFAIT <parfaitteli@outlook.fr>
2025-02-27 20:21:08 +0100

    modif new covoit

--- profil.php
+++ profil.php
@@ -4,3 +4,21 @@ include('includes/connect.php');
 include('includes/header.php');
 
 echo "BIENVENU SUR VOTRE PROFILE";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
+require_once('includes/footer.php');
+?>
