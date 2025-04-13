9204ff643c225d6259f30391d3a0085015c95e1e
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-04-13 20:55:21 +0200

    echo supprimé

--- avis.php
+++ avis.php
@@ -9,7 +9,6 @@ if ($_SESSION && $_SESSION["user"]['Id_utilisateur']) {
 }
 if ($profil) {
         // Récupérer les avis de l'utilisateur
-
         $sql = "SELECT c.date_depart, c.heure_depart, c.lieu_depart, c.lieu_arrivee, r.places_reserves
                         FROM reservation AS r
                         JOIN covoiturage AS c ON r.Id_covoiturage = c.Id_covoiturage
@@ -18,21 +17,21 @@ if ($profil) {
         $stmt->bindValue(':Id_utilisateur', $profil);
         $stmt->execute();
         $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // var_dump($avis);
-        // formulaire de saisie d'avis
-        echo '<form method="post" class="form-group"><br>';
-        echo '<div class="mb-3 text-center" id="form-controler">';
-        echo '<div class="mb-3">';
-        echo '<label for="avis" style: color="white">Votre avis</label><br>';
-        echo '<textarea id="story" name="story" rows="5" cols="33" placeholder="saisissz votre avs ici" ></textarea>';
-        echo '</div>';
-        echo '<div class="mb-3">';
-        echo '<label for="note">Votre note</label><br>';
-        echo '<input type="number" name="note" id="note" required placeholder="Votre note" min="1" max="5">';
-        echo '</div>';
-        echo '<button type="submit" class="btn btn-primary mb-3">Envoyer</button><br>';
-        echo '</div>';
-        echo '</form>';
+?>
+        <form method="post" class="form-group">
+                <div class="mb-3 text-center" id="form-controler">
+                        <div class="mb-3">
+                                <label for="avis" style: color="white" class="mb-2">Votre avis</label><br>
+                                <textarea id="story" name="story" rows="5" cols="33" placeholder="saisissz votre avs ici"></textarea>
+                        </div>
+                        <div class="mb-3">
+                                <label for="note" class="m-2">Votre note</label><br>
+                                <input type="number" name="note" id="note" required placeholder="Votre note" min="1" max="5">
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-3">Envoyer</button><br>
+                </div>
+        </form>
+<?php
         // envoi de l'avis dans la base de données
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $avis = $_POST['story'];
