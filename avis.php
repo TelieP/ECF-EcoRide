31292f792571ef4141d3695605567e275c8b2d2e
PARFAIT <parfaitteli@outlook.fr>
2025-04-13 18:39:12 +0200

    modifs pages avis

--- avis.php
+++ avis.php
@@ -9,11 +9,10 @@ if ($_SESSION && $_SESSION["user"]['Id_utilisateur']) {
 }
 if ($profil) {
         // Récupérer les avis de l'utilisateur
-        $sql = "SELECT a.commentaire, a.note, c.date_depart, c.heure_depart, c.lieu_depart, c.lieu_arrivee
-        FROM avis AS a
-        JOIN covoiturage AS c ON depose.Id_covoiturage = c.Id_covoiturage
-        JOIN participe AS p ON c.Id_covoiturage = p.Id_covoiturage
-        WHERE p.Id_utilisateur = :Id_utilisateur";
+        $sql = "SELECT c.date_depart, c.heure_depart, c.lieu_depart, c.lieu_arrivee, r.places_reserves
+                        FROM reservation AS r
+                        JOIN covoiturage AS c ON r.Id_covoiturage = c.Id_covoiturage
+                        WHERE r.Id_utilisateur = :Id_utilisateur";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':Id_utilisateur', $profil);
         $stmt->execute();
