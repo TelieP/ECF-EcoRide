4cd52c8e4af26aa8d6aecc6e47104910cadb7315
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-04-13 12:00:46 +0200

    Test session

--- avis.php
+++ avis.php
@@ -3,7 +3,9 @@ require_once 'includes/connect.php';
 require_once 'includes/header.php';
 echo " Bienvenue sur la page d'avis";
 session_start();
-$profil =  $_SESSION['Id_utilisateur'];
+if ($_SESSION && $_SESSION['Id_utilisateur']) {
+        $profil = $_SESSION['Id_utilisateur'];
+}
 // Récupérer les avis de l'utilisateur
 $sql = "SELECT a.commentaire, a.note, c.date_depart, c.heure_depart, c.lieu_depart, c.lieu_arrivee
         FROM avis AS a
