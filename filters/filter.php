ebe59da7652cb860eaf75829ad2caaad1abaa9a8
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-04-20 11:32:25 +0200

    JS OK

--- filters/filter.php
+++ filters/filter.php
@@ -28,17 +28,26 @@ if (isset($_GET['ecological'])) {
 
 
 // On récupère la variable lowprice (0 ou 1) passée dans la requête Ajax du fichier filter.js
-if (isset($_GET['askOrDesk']) && isset($_GET['idsCovoit'])) {
+if (isset($_GET['askOrDesk']) && isset($_GET['idsCovoits'])) {
   // Cocher ou pas
   $askOrDesk = $_GET['askOrDesk'] == 1 ? "ASC" : "DESC";
-  $idsCovoit = $_GET['idsCovoits'];
+  // Tableau des ids
+  $idsCovoits = $_GET['idsCovoits'];
+  // Conversion pour la requête (ajout virgule pour le IN de la query)
+  $idsForSql = '';
+  foreach ($idsCovoits as $idCovoit) {
+    $idsForSql .= $idCovoit;
+    $idsForSql .= ',';
+  }
+  // Suprimer la dernière virgule
+  $idsForSql = substr($idsForSql, 0, -1);
 
   $sql = "SELECT c.Id_covoiturage, c.heure_depart, c.date_depart, c.nb_place, u.photo,
             c.heure_arrivee, u.nom, u.prenom, v.modele, v.immatriculation, c.prix_personne, c.lieu_depart, c.lieu_arrivee
                 FROM covoiturage AS c
                 JOIN voiture AS v ON v.Id_voiture = c.Id_voiture
                 JOIN utilisateur AS u ON u.Id_utilisateur = v.Id_voiture
-                WHERE c.Id_covoiturage IN ($idsCovoit)
+                WHERE c.Id_covoiturage IN ($idsForSql)
                 ORDER BY c.prix_personne $askOrDesk";
   $stmt = $conn->prepare($sql);
   $stmt->execute();
