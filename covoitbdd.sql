c5a25a2b2c2a39f26ed62b70e25fc90dd2f2b221
PARFAIT <parfaitteli@outlook.fr>
2025-04-28 21:42:16 +0200

    mise à jour bdd

--- covoitbdd.sql
+++ covoitbdd.sql
@@ -3,42 +3,63 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 17 jan. 2025 à 10:44
+-- Généré le : lun. 28 avr. 2025 à 21:39
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de données : `covoitbdd`
 --
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `avis`
-CREATE DATABASE IF NOT EXISTS `covoitbdd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `covoitbdd`;
+--
+
 CREATE TABLE `avis` (
   `Id_avis` int(11) NOT NULL,
   `commentaire` varchar(50) DEFAULT NULL,
   `note` varchar(50) DEFAULT NULL,
-  `statut` varchar(50) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `statut` enum('En attente','Validé','Supprimé','') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`Id_avis`, `commentaire`, `note`, `statut`) VALUES
+(1, 'Le voyage s\'est très bien passé. Je recommande ce ', '5', 'En attente'),
+(2, 'bien ', '5', 'En attente'),
+(4, 'conductrice très aimable , et gentille , je recomm', '5', NULL);
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `configuration`
 --
+
 CREATE TABLE `configuration` (
   `Id_configuration` int(11) NOT NULL,
   `Id_utilisateur` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `covoiturage`
 --
+
 CREATE TABLE `covoiturage` (
   `Id_covoiturage` int(11) NOT NULL,
   `date_depart` date DEFAULT NULL,
@@ -51,61 +72,168 @@ CREATE TABLE `covoiturage` (
   `nb_place` int(11) DEFAULT NULL,
   `prix_personne` int(11) DEFAULT NULL,
   `Id_voiture` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `covoiturage`
+--
+
+INSERT INTO `covoiturage` (`Id_covoiturage`, `date_depart`, `heure_depart`, `lieu_depart`, `date_arrivee`, `heure_arrivee`, `lieu_arrivee`, `statut`, `nb_place`, `prix_personne`, `Id_voiture`) VALUES
+(2, '2025-03-12', '10:20', 'tolouse', '2025-03-25', '18:55', 'paris', 'disponible', 4, 55, 1),
+(3, '2025-02-27', '09:10', 'Lyon', '2025-02-27', '14:25', 'Narbonne', 'Disponible', 2, 20, 2),
+(4, '2025-03-23', '10:20', 'narbonne', '2025-03-23', '18:55', 'paris', 'disponible', 4, 55, 2),
+(5, '2025-03-23', '08:12', 'narbonne', '2025-03-23', '14:25', 'paris', 'disponible', 0, 30, 1),
+(6, '2025-04-24', '10:20', 'lyon', '2025-04-24', '14:25', 'bordeaux', 'disponible', 3, 22, 2),
+(7, '2025-04-15', '07:05', 'Perpignan', '2025-04-15', '14:25', 'Pau', 'Disponible', 2, 26, 2),
+(8, '2025-04-15', '10:20', 'brest', '2025-04-15', '14:25', 'paris', 'disponible', 4, 30, 3),
+(9, '2025-04-15', '09:10', 'brest', '2025-04-15', '14:25', 'paris', 'Disponible', 2, 20, 1),
+(10, '2025-04-15', '06:00', 'brest', '2025-04-15', '9:25', 'paris', 'disponible', 2, 24, 2);
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `depose`
 --
+
 CREATE TABLE `depose` (
   `Id_utilisateur` int(11) NOT NULL,
   `Id_avis` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `marque`
 --
+
 CREATE TABLE `marque` (
   `Id_marque` int(11) NOT NULL,
   `libelle` varchar(50) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `marque`
+--
+
+INSERT INTO `marque` (`Id_marque`, `libelle`) VALUES
+(1, 'Toyota'),
+(2, 'Nissan'),
+(3, 'Renault'),
+(4, 'Peugeot'),
+(5, 'Hundai'),
+(6, 'Kia'),
+(7, 'Mercedez'),
+(8, 'Porshe'),
+(9, 'Range rover'),
+(10, 'Audi'),
+(11, 'BMW'),
+(12, 'Fiat'),
+(13, 'Dacia'),
+(14, 'Krisler'),
+(15, 'BYD'),
+(16, 'Testla'),
+(17, 'BW'),
+(18, 'Opel'),
+(19, 'Suziki'),
+(20, 'Ford'),
+(21, 'Honda');
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `parametre`
 --
+
 CREATE TABLE `parametre` (
   `Id_parametre` int(11) NOT NULL,
   `propriete` varchar(50) DEFAULT NULL,
   `valeur` varchar(50) DEFAULT NULL,
   `Id_configuration` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `participe`
 --
+
 CREATE TABLE `participe` (
   `Id_utilisateur` int(11) NOT NULL,
   `Id_covoiturage` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `possede`
 --
+
 CREATE TABLE `possede` (
   `Id_role` int(11) NOT NULL,
   `Id_utilisateur` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `possede`
+--
+
+INSERT INTO `possede` (`Id_role`, `Id_utilisateur`) VALUES
+(2, 8);
+
 -- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `Id_reservation` int(255) NOT NULL,
+  `Id_utilisateur` int(100) NOT NULL,
+  `Id_covoiturage` int(11) NOT NULL,
+  `places_reserves` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`Id_reservation`, `Id_utilisateur`, `Id_covoiturage`, `places_reserves`) VALUES
+(1, 4, 5, 2),
+(2, 7, 5, 1),
+(3, 7, 5, 1),
+(4, 7, 5, 1),
+(5, 7, 5, 1),
+(6, 7, 5, 1),
+(7, 7, 5, 1),
+(8, 7, 4, 3),
+(9, 7, 4, 1),
+(10, 7, 5, 1);
+
+-- --------------------------------------------------------
+
 --
 -- Structure de la table `role`
 --
+
 CREATE TABLE `role` (
   `Id_role` int(11) NOT NULL,
   `libelle` varchar(50) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`Id_role`, `libelle`) VALUES
+(2, 'user'),
+(3, 'employé');
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `utilisateur`
 --
+
 CREATE TABLE `utilisateur` (
   `Id_utilisateur` int(11) NOT NULL,
   `nom` varchar(50) DEFAULT NULL,
@@ -117,50 +245,26 @@ CREATE TABLE `utilisateur` (
   `date_naissance` varchar(50) DEFAULT NULL,
   `photo` varchar(50) DEFAULT NULL,
   `pseudo` varchar(50) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Déchargement des données de la table `utilisateur`
 --
-INSERT INTO `utilisateur` (
-    `Id_utilisateur`,
-    `nom`,
-    `prenom`,
-    `email`,
-    `password`,
-    `telephone`,
-    `adresse`,
-    `date_naissance`,
-    `photo`,
-    `pseudo`
-  )
-VALUES (
-    1,
-    'FONGANG Telie',
-    'Parfait',
-    'parfaittelie@outlook.fr',
-    '$argon2id$v=19$m=65536,t=4,p=1$bkgwc21jdXZpbVV0eDdVYg$Pgss6LxWO1rr3jaYjezrq5NOkiMzXpIV+jH2xPgDKiA',
-    '0615487341',
-    '53 avenue georges clemenceau',
-    '1980-09-24',
-    '',
-    'boubou'
-  ),
-  (
-    2,
-    'bouzina',
-    'anais',
-    'anais@gmail.com',
-    '$argon2id$v=19$m=65536,t=4,p=1$LzkxZGwyNmZ0b283cVdPRQ$AOH/MXKdIXwxfJGez59JbFz3ZDBS3JHLtvBIyuG7np4',
-    '41245789',
-    '53 avenue georges clemenceau',
-    '1989-10-23',
-    '',
-    'anais10'
-  );
+
+INSERT INTO `utilisateur` (`Id_utilisateur`, `nom`, `prenom`, `email`, `password`, `telephone`, `adresse`, `date_naissance`, `photo`, `pseudo`) VALUES
+(1, 'FONGANG Telie', 'Parfait', 'paflesix@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$MkJEUHFCNmJhOUV5dTMycA$YiaCSsIgaFrnis7KOvKGEfmB+QGAiJeQhhIaNpmJ9Gc', '0615487341', '53 avenue georges clemenceau', '2000-12-12', 'images/conducteurs/imguser01.jpg', 'baba'),
+(2, 'baba', 'bobo', 'parfait@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$RVhmWC9Fb3ZKbVA4TmZPVA$Hoh3rXek+IBeOq87tR5HerLwdXasXWigSP1suWJdDuk', '0611251436', '2bis rue renan 66000 perpignan', '1985-12-22', '../images/conducteurs/imguser03.jpg', 'bobolo'),
+(3, 'telie', 'papa', 'papa@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$RDRBd3JPVlJ6OUFicnRWUA$Mka1WvMuzRsnuPPuTqLcCayGRYHhbzcmZ0MP1E3TNS0', '12478812420', '12 rue paul ', '2000-12-12', '../images/conducteurs/imguser02.jpg', 'prince'),
+(4, 'Bouzina', 'Anais', 'anais@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$bVMvelEvNi9XLzVsYW9uYw$Bm9BbqrCMFp4oRj/szbOu1GgB/9jF39+wNG02sWYvJQ', '0615487341', '53 avenue georges clemenceau', '1980-12-12', 'images/imguser01.jpg', 'anais92'),
+(7, 'Mamon', 'marie', 'marie@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$L1VDQ2Y5dHJBL2JneUtOSA$h+eoXSL4BOqL421+AJsLHvDK5brNiIv3Gp6/O25dTJI', '0712131415', '69 rue paul gautier', '2000-09-23', 'images/conducteurs/imguser03.jpg', 'marilou'),
+(8, 'nana', 'nana', 'nana0000@gmail.fr', '$argon2id$v=19$m=65536,t=4,p=1$YWx1T3Bac2twa1BFWS5TZw$FtuRUnndZ09RFTMPVN18FocO+fU4GZxFZcdSYu4QQIQ', '0689472514', '12 rue paul 95000 Paris ', '1990-06-12', 'nana.jpg', 'nanou');
+
 -- --------------------------------------------------------
+
 --
 -- Structure de la table `voiture`
 --
+
 CREATE TABLE `voiture` (
   `Id_voiture` int(11) NOT NULL,
   `modele` varchar(50) DEFAULT NULL,
@@ -170,160 +274,219 @@ CREATE TABLE `voiture` (
   `date_premiere_immatriculation` varchar(50) DEFAULT NULL,
   `Id_utilisateur` int(11) NOT NULL,
   `Id_marque` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `voiture`
+--
+
+INSERT INTO `voiture` (`Id_voiture`, `modele`, `immatriculation`, `energie`, `couleur`, `date_premiere_immatriculation`, `Id_utilisateur`, `Id_marque`) VALUES
+(1, 'toyota', 'BH-485-GR', 'essence', 'rouge', '12/06/2024', 1, 1),
+(2, 'nissan', 'JK-364-PO', 'Diesel', 'noire', '30/01/2007', 2, 2),
+(3, 'modelo', 'BJ-160-MX', 'Electrique', 'Blue', '12/02/2025', 4, 8);
+
 --
 -- Index pour les tables déchargées
 --
+
 --
 -- Index pour la table `avis`
 --
 ALTER TABLE `avis`
-ADD PRIMARY KEY (`Id_avis`);
+  ADD PRIMARY KEY (`Id_avis`);
+
 --
 -- Index pour la table `configuration`
 --
 ALTER TABLE `configuration`
-ADD PRIMARY KEY (`Id_configuration`),
+  ADD PRIMARY KEY (`Id_configuration`),
   ADD KEY `Id_utilisateur` (`Id_utilisateur`);
+
 --
 -- Index pour la table `covoiturage`
 --
 ALTER TABLE `covoiturage`
-ADD PRIMARY KEY (`Id_covoiturage`),
+  ADD PRIMARY KEY (`Id_covoiturage`),
   ADD KEY `Id_voiture` (`Id_voiture`);
+
 --
 -- Index pour la table `depose`
 --
 ALTER TABLE `depose`
-ADD PRIMARY KEY (`Id_utilisateur`, `Id_avis`),
+  ADD PRIMARY KEY (`Id_utilisateur`,`Id_avis`),
   ADD KEY `Id_avis` (`Id_avis`);
+
 --
 -- Index pour la table `marque`
 --
 ALTER TABLE `marque`
-ADD PRIMARY KEY (`Id_marque`);
+  ADD PRIMARY KEY (`Id_marque`);
+
 --
 -- Index pour la table `parametre`
 --
 ALTER TABLE `parametre`
-ADD PRIMARY KEY (`Id_parametre`),
+  ADD PRIMARY KEY (`Id_parametre`),
   ADD KEY `Id_configuration` (`Id_configuration`);
+
 --
 -- Index pour la table `participe`
 --
 ALTER TABLE `participe`
-ADD PRIMARY KEY (`Id_utilisateur`, `Id_covoiturage`),
+  ADD PRIMARY KEY (`Id_utilisateur`,`Id_covoiturage`),
   ADD KEY `Id_covoiturage` (`Id_covoiturage`);
+
 --
 -- Index pour la table `possede`
 --
 ALTER TABLE `possede`
-ADD PRIMARY KEY (`Id_role`, `Id_utilisateur`),
+  ADD PRIMARY KEY (`Id_role`,`Id_utilisateur`),
   ADD KEY `Id_utilisateur` (`Id_utilisateur`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`Id_reservation`),
+  ADD KEY `Id_utilisateur` (`Id_utilisateur`),
+  ADD KEY `Id_covoiturage` (`Id_covoiturage`);
+
 --
 -- Index pour la table `role`
 --
 ALTER TABLE `role`
-ADD PRIMARY KEY (`Id_role`);
+  ADD PRIMARY KEY (`Id_role`);
+
 --
 -- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-ADD PRIMARY KEY (`Id_utilisateur`);
+  ADD PRIMARY KEY (`Id_utilisateur`);
+
 --
 -- Index pour la table `voiture`
 --
 ALTER TABLE `voiture`
-ADD PRIMARY KEY (`Id_voiture`),
+  ADD PRIMARY KEY (`Id_voiture`),
   ADD KEY `Id_utilisateur` (`Id_utilisateur`),
   ADD KEY `Id_marque` (`Id_marque`);
+
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
 --
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
-MODIFY `Id_avis` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_avis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT pour la table `configuration`
 --
 ALTER TABLE `configuration`
-MODIFY `Id_configuration` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_configuration` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT pour la table `covoiturage`
 --
 ALTER TABLE `covoiturage`
-MODIFY `Id_covoiturage` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_covoiturage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT pour la table `marque`
 --
 ALTER TABLE `marque`
-MODIFY `Id_marque` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_marque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
 --
 -- AUTO_INCREMENT pour la table `parametre`
 --
 ALTER TABLE `parametre`
-MODIFY `Id_parametre` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_parametre` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `Id_reservation` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT pour la table `role`
 --
 ALTER TABLE `role`
-MODIFY `Id_role` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-MODIFY `Id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
+  MODIFY `Id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT pour la table `voiture`
 --
 ALTER TABLE `voiture`
-MODIFY `Id_voiture` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_voiture` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- Contraintes pour les tables déchargées
 --
+
 --
 -- Contraintes pour la table `configuration`
 --
 ALTER TABLE `configuration`
-ADD CONSTRAINT `configuration_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`);
+  ADD CONSTRAINT `configuration_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`);
+
 --
 -- Contraintes pour la table `covoiturage`
 --
 ALTER TABLE `covoiturage`
-ADD CONSTRAINT `covoiturage_ibfk_1` FOREIGN KEY (`Id_voiture`) REFERENCES `voiture` (`Id_voiture`);
+  ADD CONSTRAINT `covoiturage_ibfk_1` FOREIGN KEY (`Id_voiture`) REFERENCES `voiture` (`Id_voiture`);
+
 --
 -- Contraintes pour la table `depose`
 --
 ALTER TABLE `depose`
-ADD CONSTRAINT `depose_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
+  ADD CONSTRAINT `depose_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
   ADD CONSTRAINT `depose_ibfk_2` FOREIGN KEY (`Id_avis`) REFERENCES `avis` (`Id_avis`);
+
 --
 -- Contraintes pour la table `parametre`
 --
 ALTER TABLE `parametre`
-ADD CONSTRAINT `parametre_ibfk_1` FOREIGN KEY (`Id_configuration`) REFERENCES `configuration` (`Id_configuration`);
+  ADD CONSTRAINT `parametre_ibfk_1` FOREIGN KEY (`Id_configuration`) REFERENCES `configuration` (`Id_configuration`);
+
 --
 -- Contraintes pour la table `participe`
 --
 ALTER TABLE `participe`
-ADD CONSTRAINT `participe_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
+  ADD CONSTRAINT `participe_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
   ADD CONSTRAINT `participe_ibfk_2` FOREIGN KEY (`Id_covoiturage`) REFERENCES `covoiturage` (`Id_covoiturage`);
+
 --
 -- Contraintes pour la table `possede`
 --
 ALTER TABLE `possede`
-ADD CONSTRAINT `possede_ibfk_1` FOREIGN KEY (`Id_role`) REFERENCES `role` (`Id_role`),
+  ADD CONSTRAINT `possede_ibfk_1` FOREIGN KEY (`Id_role`) REFERENCES `role` (`Id_role`),
   ADD CONSTRAINT `possede_ibfk_2` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`);
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`Id_covoiturage`) REFERENCES `covoiturage` (`Id_covoiturage`);
+
 --
 -- Contraintes pour la table `voiture`
 --
 ALTER TABLE `voiture`
-ADD CONSTRAINT `voiture_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
+  ADD CONSTRAINT `voiture_ibfk_1` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`Id_utilisateur`),
   ADD CONSTRAINT `voiture_ibfk_2` FOREIGN KEY (`Id_marque`) REFERENCES `marque` (`Id_marque`);
 COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
