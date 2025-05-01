158200cba17f3078eb5639f644fcf420859a7906
PARFAIT <parfaitteli@outlook.fr>
2025-05-01 22:42:10 +0200

    ajout de condition pour afficher avis ou non dans la page profil.php

--- profil.php
+++ profil.php
@@ -10,16 +10,6 @@ include 'includes/connect.php';
 include 'includes/header.php';
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil</title>
-
-</head>
-
 
 <body>
     <h1>Mon Profil</h1>
