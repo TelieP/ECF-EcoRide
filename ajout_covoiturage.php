ce4c905f0c06bb0c304cdad782efe25bd161765f
PARFAIT <parfaitteli@outlook.fr>
2025-05-03 22:52:46 +0200

    mise à  ajout covoit

--- ajout_covoiturage.php
+++ ajout_covoiturage.php
@@ -25,7 +25,17 @@ if (isset($_SESSION['user'])) {
         $prix = $_POST['prix'];
         //on recupere les infos du formulaire et on les stock en base de données
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $stmt = $conn->prepare("INSERT INTO `covoiturage` (`date_depart`, `heure_depart`, `lieu_depart`, `lieu_arrivee`, `statut`, `nb_place`, `prix_personne`) VALUES (:date_depart, :heure_depart, :lieu_depart, :lieu_arrivee, :statut, :nb_place, :prix)");
+
+            $sql = "SELECT Id_voiture
+            FROM voiture AS v
+            JOIN utilisateur AS u ON u.Id_utilisateur = v.Id_utilisateur
+            WHERE u.Id_utilisateur = $_SESSION(['user']['Id_utilisateur'])";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':Id_utilisateur', $_SESSION(['user']['Id_utilisateur']), PDO::PARAM_INT);
+            $stmt->execute();
+            $voiture = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $stmt = $conn->prepare("INSERT INTO `covoiturage` (`date_depart`, `heure_depart`, `lieu_depart`, `lieu_arrivee`, `statut`, `nb_place`, `prix_personne`,Id_voiture) VALUES (:date_depart, :heure_depart, :lieu_depart, :lieu_arrivee, :statut, :nb_place, :prix, :Id_voiture)");
             $stmt->bindParam(':lieu_depart', $depart, PDO::PARAM_STR);
             $stmt->bindParam(':lieu_arrivee', $arrivee, PDO::PARAM_STR);
             $stmt->bindParam(':date_depart', $date, PDO::PARAM_STR);
@@ -33,6 +43,8 @@ if (isset($_SESSION['user'])) {
             $stmt->bindParam(':nb_place', $nb_place, PDO::PARAM_INT);
             $stmt->bindParam(':statut', $statut, PDO::PARAM_STR);
             $stmt->bindParam(':prix', $prix, PDO::PARAM_INT);
+            $stmt->bindParam(':Id_voiture', $_POST['voiture'], PDO::PARAM_INT);
+            $stmt->bindParam(':Id_utilisateur', $_SESSION['user']['Id_utilisateur'], PDO::PARAM_INT);
             if ($stmt->execute()) {
                 echo "Covoiturage ajouté avec succès";
             } else {
