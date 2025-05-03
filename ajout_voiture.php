e7879d9c607d9b6b130611eea1dd534d3a4b5021
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-05-03 20:22:42 +0200

    correction

--- ajout_voiture.php
+++ ajout_voiture.php
@@ -18,8 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $preferences = $_POST['preferences'];
 
     $sql = "INSERT INTO voiture (Id_utilisateur, marque, modele, date_mise_en_circulation, immatriculation, energie, preferences, libelle) 
-            VALUES (:Id_utilisateur, :marque, :modele, :date_mise_en_circulation, :immatriculation, :energie, :preferences, :libelle)
-             JOIN marque ON voiture.Id_marque = marque.Id_marque";
+            VALUES (:Id_utilisateur, :marque, :modele, :date_mise_en_circulation, :immatriculation, :energie, :preferences, :libelle)";
     $stmt = $conn->prepare($sql);
 
 
