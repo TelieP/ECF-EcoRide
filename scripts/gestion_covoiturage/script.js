5f4b2f5e6d77d9390c3d8f6f6a2bcee643bc3a55
PARFAIT <parfaitteli@outlook.fr>
2025-05-08 13:58:46 +0200

    mise à jour fichier script.js

--- scripts/gestion_covoiturage/script.js
+++ scripts/gestion_covoiturage/script.js
@@ -6,29 +6,20 @@
     function toggleButton() {
         // on recupère l'id du bouton
         const button = document.getElementById('toggleButton');
-        // si le texte du bouton est "Démarer" on le change en "Arrêter"
-        if (button.innerHTML === "Démarrer") {
-            button.innerHTML = "Arrêter";
-            // on change la couleur du bouton en rouge
-            button.style.backgroundColor = "red";
-            // sendEmail();
-        } else {
-            // sinon on le remet en "Démarer"
-            button.innerHTML = "Démarrer";
-            // on remet la couleur du bouton en vert
-            button.style.backgroundColor = "green";
-        }
+        button.addEventListener('click', function() {
+          alert("Le trajet a démarré passez un bon voyage!");
+        });
     }
     toggleButton();
     
-    function sendEmail() {
-        // création  de l'UrL mailto pour envoyer un mail au covoitureur
-        const mailtoLink = "mailto:destinataire@exmple.com?subject=Objet du mail&body=Corps du mail";
-        // on ouvre l'application de messagerie de l'utlisateur pour envoyer le mail
-        windows.location.href = mailtoLink;
-        // on affiche un message de confirmation
-        alert("Un mail a été envoyé au covoitureur !");
-    }
+    // function sendEmail() {
+    //     // création  de l'UrL mailto pour envoyer un mail au covoitureur
+    //     const mailtoLink = "mailto:destinataire@exmple.com?subject=Objet du mail&body=Corps du mail";
+    //     // on ouvre l'application de messagerie de l'utlisateur pour envoyer le mail
+    //     windows.location.href = mailtoLink;
+    //     // on affiche un message de confirmation
+    //     alert("Un mail a été envoyé au covoitureur !");
+    // }
 
   
 
