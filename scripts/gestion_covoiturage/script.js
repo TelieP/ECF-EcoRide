a19ecb0b0661cdeab7b042b0badd2b62fb1595b7
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-05-08 21:11:34 +0200

    dossiers scripts

--- scripts/gestion_covoiturage/script.js
+++ scripts/gestion_covoiturage/script.js
@@ -3,25 +3,34 @@
 
 // fonction qui permet créer un bouton démarer pour chaque trajet et envoyer un mail au covoitureur
 
+
     function toggleButton() {
         // on recupère l'id du bouton
         const button = document.getElementById('toggleButton');
-        // si le texte du bouton est "Démarer" on le change en "Arrêter"
-        if (button.innerHTML === "Démarer") {
-            button.innerHTML = "Arrêter";
-            // on change la couleur du bouton en rouge
-            button.style.backgroundColor = "red";
-            sendEmail();
-        } else {
-            // sinon on le remet en "Démarer"
-            button.innerHTML = "Démarer";
-            // on remet la couleur du bouton en vert
-            button.style.backgroundColor = "green";
-        }
-        button.addEventListener('click', () => {
+        button.addEventListener('click', function() {
           alert("Le trajet a démarré passez un bon voyage!");
         });
     }
+    // toggleButton();
+    // function toggleButton() {
+    //     // on recupère l'id du bouton
+    //     const button = document.getElementById('toggleButton');
+    //     // si le texte du bouton est "Démarer" on le change en "Arrêter"
+    //     if (button.innerHTML === "Démarer") {
+    //         button.innerHTML = "Arrêter";
+    //         // on change la couleur du bouton en rouge
+    //         button.style.backgroundColor = "red";
+    //         sendEmail();
+    //     } else {
+    //         // sinon on le remet en "Démarer"
+    //         button.innerHTML = "Démarer";
+    //         // on remet la couleur du bouton en vert
+    //         button.style.backgroundColor = "green";
+    //     }
+    //     button.addEventListener('click', () => {
+    //       alert("Le trajet a démarré passez un bon voyage!");
+    //     });
+    // }
     toggleButton();
     
     function sendEmail() {
