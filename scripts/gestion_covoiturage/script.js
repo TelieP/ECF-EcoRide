21319cac31d572e3f6edbd7cd9072fd0315e2d7a
PARFAIT <parfaitteli@outlook.fr>
2025-05-08 13:34:46 +0200

    mise à jour fichier script.js

--- scripts/gestion_covoiturage/script.js
+++ scripts/gestion_covoiturage/script.js
@@ -7,14 +7,14 @@
         // on recupère l'id du bouton
         const button = document.getElementById('toggleButton');
         // si le texte du bouton est "Démarer" on le change en "Arrêter"
-        if (button.innerHTML === "Démarer") {
+        if (button.innerHTML === "Démarrer") {
             button.innerHTML = "Arrêter";
             // on change la couleur du bouton en rouge
             button.style.backgroundColor = "red";
-            sendEmail();
+            // sendEmail();
         } else {
             // sinon on le remet en "Démarer"
-            button.innerHTML = "Démarer";
+            button.innerHTML = "Démarrer";
             // on remet la couleur du bouton en vert
             button.style.backgroundColor = "green";
         }
@@ -29,6 +29,6 @@
         // on affiche un message de confirmation
         alert("Un mail a été envoyé au covoitureur !");
     }
-// executer la fonction toggleButton() 
+
   
 
