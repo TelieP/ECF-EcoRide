c85f01ca7abe6702ede210726d0472c01e4807ed
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-05-09 00:01:09 +0200

    js

--- scripts/gestion_covoiturage/script.js
+++ scripts/gestion_covoiturage/script.js
@@ -1,9 +1,10 @@
 // fonction qui permet de démarrer un trajet
 function startCovoit() {
-    const startButtons = document.getElementsByClassName('start_cov');
+    const startButtons = document.querySelectorAll('.start_cov'); // renvoie un NodeList
     startButtons.forEach((startButton) => {
         startButton.addEventListener('click', () => {
-            alert("Le trajet a démarré passez un bon voyage!");
-        })
-    })
+            alert("Le trajet a démarré, passez un bon voyage!");
+        });
+    });
 }
+startCovoit();
