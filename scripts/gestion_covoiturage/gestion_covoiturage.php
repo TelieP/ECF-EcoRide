8cabb51ab11cde960581d22caf7f115c72776fa5
Nicolas Deroo <42850170+deroonicolas@users.noreply.github.com>
2025-05-10 16:08:04 +0200

    changement statut cvoit

--- scripts/gestion_covoiturage/gestion_covoiturage.php
+++ scripts/gestion_covoiturage/gestion_covoiturage.php
@@ -8,17 +8,18 @@ require_once '../../includes/connect.php';
 if (isset($_POST['startOrStop'])) {
   // On récupère "D" ou "T"
   $startOrStop = $_POST['startOrStop'];
-  // On met à la jour la colonne statut de la table covoiturage de la DB
+  // On met à la jour la colonne statut de la table covoiturage de la DB ('dispobible' ou 'en cours')
   $query = "UPDATE covoiturage SET statut = :statut WHERE id = :id";
   $stmt = $conn->prepare($query);
   if ($startOrStop == 'D') {
+    // Le vocovoit a démarré
     $sql = "UPDATE covoiturage
-SET statut = REPLACE(statut, 'disponible', 'en cours')";
+      SET statut = REPLACE(statut, 'disponible', 'en cours')";
   } else {
+    // Le covoit est terminé
     $sql = "UPDATE covoiturage  
-SET statut = REPLACE(statut, 'en cours', 'terminé')";
+      SET statut = REPLACE(statut, 'en cours', 'terminé')";
   }
-  // TODO Le faire
-  // Test du retour : à commenter quand le TODO sera terminé
+  // On revoie au JS "D" ou "T"
   echo json_encode($startOrStop);
 }
