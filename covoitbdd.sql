d15a4175f5bd344875d903bb36ad183ad47604a9
PARFAIT <parfaitteli@outlook.fr>
2025-05-16 21:45:28 +0200

    suppression des commentaires et MAJ de la bdd

--- covoitbdd.sql
+++ covoitbdd.sql
@@ -1,8 +1,8 @@
 
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS `covoitbdd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `covoitbdd`;
+
 
 CREATE TABLE `avis` (
   `Id_avis` int(11) NOT NULL,
